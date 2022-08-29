f24bd333cf7ad4d7761b82e11adc3994dbc0fc93
Simon Martineau <simon.martineau.3@ulaval.ca>
2022-08-29 16:52:39 -0400

    Base body

--- src/style/styled-commons.ts
+++ src/style/styled-commons.ts
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 export const SContainer = styled.div`
-  width: 70%;
+  width: 80%;
   margin: 0 auto;
+`;
+
+export const SSection = styled.div`
+  background: #e5ffff;
+  border-radius: 15px;
+  padding: 16px;
 `;
