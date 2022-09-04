d3b19979ad11dbd5af29af0a67ae7467cb4f352f
Simon Martineau <simon.martineau.3@ulaval.ca>
2022-09-04 17:59:05 -0400

    Find fishies button with animation

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,7 +1,4 @@
-import {
-  faQuestion,
-  faQuestionCircle,
-} from "@fortawesome/free-solid-svg-icons";
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import { SContainer, SSection } from "../../style/styled-commons";
--- src/components/FishSection/FishSection.tsx
+++ src/components/FishSection/FishSection.tsx
@@ -1,6 +1,18 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import StreamSection from "../StreamSection";
+
+const SArrowFrames = keyframes`
+  from {
+    padding-left: 0;
+  }
+
+  to {
+    padding-left: 12px;
+  }
+`;
 
 const SGridWrapper = styled.div`
   display: grid;
@@ -10,12 +22,47 @@ const SGridWrapper = styled.div`
   align-items: center;
 `;
 
+const SConvertButton = styled.button`
+  background: #17347e;
+  font-family: inherit;
+  font-size: 1rem;
+  border: none;
+  padding: 8px;
+  border-radius: 10px;
+  color: #eee;
+  cursor: pointer;
+
+  &:hover {
+    background: #43588f;
+    transition: background 0.2s ease-in;
+
+    & > #animation-target {
+      animation: ${SArrowFrames} 0.4s ease-in-out alternate infinite;
+    }
+  }
+`;
+
+const SArrowWrapper = styled.div`
+  display: inline-block;
+  width: 40px;
+`;
+
 export default function FishSection() {
   return (
     <SGridWrapper>
       <StreamSection />
-      <div style={{ backgroundColor: "red" }}>asdf</div>
-      <div style={{ backgroundColor: "blue" }}>asdf</div>
+      <SConvertButton>
+        Find fishies
+        <SArrowWrapper id="animation-target">
+          <FontAwesomeIcon
+            icon={faArrowRight}
+            style={{ color: "#FF6B00", fontSize: "1.2rem" }}
+          />
+        </SArrowWrapper>
+      </SConvertButton>
+      <div style={{ backgroundColor: "blue" }}>
+        This section will contain the resulting image
+      </div>
     </SGridWrapper>
   );
 }
